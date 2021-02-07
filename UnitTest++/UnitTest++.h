66a7903f1d84209dc13310febb23db97f6812d2b
Patrick Johnmeyer <pjohnmeyer@gmail.com>
2021-02-06 20:50:21 -0600

    Move RequireMacros into UnitTest++.h

--- UnitTest++/UnitTest++.h
+++ UnitTest++/UnitTest++.h
@@ -969,6 +969,15 @@ namespace UnitTest {
    };
 }
 
+#define UNITTEST_REQUIRE for(UnitTest::RequiredCheckTestReporter decoratedReporter(*UnitTest::CurrentTest::Results()); decoratedReporter.Next(); )
+
+#ifndef UNITTEST_DISABLE_SHORT_MACROS
+   #ifdef REQUIRE
+      #error REQUIRE already defined, re-configure with UNITTEST_ENABLE_SHORT_MACROS set to 0 and use UNITTEST_REQUIRE instead
+   #else
+      #define REQUIRE UNITTEST_REQUIRE
+   #endif
+#endif
 
 #endif
 
